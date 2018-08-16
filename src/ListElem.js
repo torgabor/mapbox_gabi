c87ec823dd517c004f4278f96017030171c942ea
torgabor@yahoo.com <torgabor@yahoo.com>
2018-08-16 23:15:34 +0200

    listelem

--- src/ListElem.js
+++ src/ListElem.js
@@ -6,26 +6,15 @@ class ListElem extends Component {
   render() {
     //conditional rendering of the sidebar list items
     //the condition matches the rendered location object's id with the currently active object in the state of App.js
-    let isActive = false;
-    if (this.props.locations.id === this.props.activeObj) {
-      isActive = true;
-    }
-    const handleClickEvt = e => {
-      this.props.handleClick(e, this.props.id);
-    };
-    return isActive ? (
+    let isActive = this.props.locations.id === this.props.activeObj;
+
+    return (
       <li
-        className="active-li"
+        className={isActive ? 'active-li' : 'inactive-li'}
         key={this.props.locations.id}
-        onClick={handleClickEvt}
-      >
-        {this.props.locations.place}
-      </li>
-    ) : (
-      <li
-        className="inactive-li"
-        key={this.props.locations.id}
-        onClick={handleClickEvt}
+        onClick={e => {
+          this.props.handleClick(e, this.props.id);
+        }}
       >
         {this.props.locations.place}
       </li>
